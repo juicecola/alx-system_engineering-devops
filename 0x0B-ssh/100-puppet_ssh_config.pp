01273282843610784ee9e50fa6a48065171e06d4
Eric Munyi <irerieric@gmail.com>
2023-03-25 04:39:01 -0700

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,17 @@
-#making changes to configuration file
-file_line { 'turnoff passwd auth':
-  ensure => 'present',
+include stdlib
+# set up your client SSH configuration file so that you can connect to a server without typing a password.
+file { '/root/.ssh/config':
+  ensure => 'file',
+  mode   => '0600',
+  content => "Host ubuntu\n\tHostName 54.172.10.124\n\tUser root\n\tIdentityFile /root/.ssh/school\n",
+}
+
+file_line { 'Turn off passwd auth':
   path   => '/etc/ssh/ssh_config',
-  line   =>  'PasswordAuthetication no', 
+  line   => '    PasswordAuthentication no',
 }
 
-file_line { 'declare the identity file':
-  ensure => 'present',
-  path   =>  '~/ssh/school',
-  line   =>  '    identity file ~/ssh/school',
+file_line { 'Declare identity file':
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
-
