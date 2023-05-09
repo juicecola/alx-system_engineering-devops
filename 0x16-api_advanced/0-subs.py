e030129af785f4d3514ecc1534dafd831ecc0b00
Eric Munyi <irerieric@gmail.com>
2023-05-09 13:37:44 -0700

    cript to retrieve the no of subs on a subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!usr/bin/python3
+
+"""number of subcribers for a given subreddit."""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": 'Codezero'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
+        return 0
